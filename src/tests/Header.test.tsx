5dd602bf7bf0091b5bd6f3c5f72bae4fed112812
DamasDebora <damasdebora32@gmail.com>
2023-10-25 18:18:04 -0300

    feat: req 8

---
+++ src/tests/Header.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Header from '../components/Header';
+import { BrowserRouter } from 'react-router-dom';
+
+describe('Header Component', () => {
+  it('checks if the profile icon redirects to the profile page', async () => {
+    render(<BrowserRouter><Header/></BrowserRouter> )
+    const profileButton = screen.getByTestId('profile-top-btn');
+    userEvent.click(profileButton);
+    await screen.findByText('Profile');
+
+    expect(screen.getByTestId('page-title').textContent).toBe('Profile');
+  });
+
+  it('hide search button when route is /profile', () => {
+    render(<BrowserRouter><Header/></BrowserRouter> )
+
+    const profileButton = screen.getByTestId('profile-top-btn');
+    profileButton.click();
+    expect(screen.queryByTestId('search-top-btn')).toBeNull();
+  });
+
+
+});
