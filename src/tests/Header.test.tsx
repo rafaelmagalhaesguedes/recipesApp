ec1fa7dfdcdfe2a5b70c354a5c56b070544f3b98
DamasDebora <damasdebora32@gmail.com>
2023-10-26 10:18:17 -0300

    fix: test

--- src/tests/Header.test.tsx
+++ src/tests/Header.test.tsx
@@ -1,6 +1,5 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { BrowserRouter } from 'react-router-dom';
 import Header from '../components/Header';
 import renderWithRouter from '../utils/renderWithRouter';
 
@@ -10,7 +9,8 @@ describe('Header Component', () => {
   const DRINKS_ROUTE = '/drinks';
   const DONE_RECIPES_ROUTE = '/done-recipes';
   const FAVORITE_RECIPES_ROUTE = '/favorite-recipes';
-  
+  const titleTestId = 'page-title';
+  const btnSearchId = 'search-top-btn';
 
   it('checks if the profile icon redirects to the profile page', () => {
     renderWithRouter(<Header />, { route: PROFILE_ROUTE });
@@ -18,8 +18,8 @@ describe('Header Component', () => {
     userEvent.click(profileButton);
     screen.findByText('Profile');
 
-    const pageTitle = screen.getByTestId('page-title').textContent;
-    expect(pageTitle).toBe('Profile');
+    const title = screen.getByTestId(titleTestId);
+    expect(title).toBeInTheDocument();
   });
 
   it('hide search button when route is /profile', () => {
@@ -27,12 +27,12 @@ describe('Header Component', () => {
 
     const profileButton = screen.getByTestId('profile-top-btn');
     profileButton.click();
-    expect(screen.queryByTestId('search-top-btn')).toBeNull();
+    expect(screen.queryByTestId(btnSearchId)).toBeNull();
   });
 
   it('search bar appears', () => {
     renderWithRouter(<Header />, { route: MEALS_ROUTE });
-    const searchButton = screen.getByTestId('search-top-btn');
+    const searchButton = screen.getByTestId(btnSearchId);
 
     fireEvent.click(searchButton);
 
@@ -44,24 +44,27 @@ describe('Header Component', () => {
 
   it('search button doesnt appear', () => {
     renderWithRouter(<Header />, { route: PROFILE_ROUTE });
-    expect(screen.queryByTestId('search-top-btn')).toBeNull();
+    expect(screen.queryByTestId(btnSearchId)).toBeNull();
   });
 
   it('test route drinks', () => {
     renderWithRouter(<Header />, { route: DRINKS_ROUTE });
-    const title = screen.getByTestId('page-title');
+    const title = screen.getByTestId(titleTestId);
+
     expect(title).toBeInTheDocument();
   });
 
   it('test route /done-recipes', () => {
     renderWithRouter(<Header />, { route: DONE_RECIPES_ROUTE });
-    const title = screen.getByTestId('page-title');
+    const title = screen.getByTestId(titleTestId);
+
     expect(title).toBeInTheDocument();
   });
 
   it('test route /favorite-recipes', () => {
     renderWithRouter(<Header />, { route: FAVORITE_RECIPES_ROUTE });
-    const title = screen.getByTestId('page-title');
+    const title = screen.getByTestId(titleTestId);
+
     expect(title).toBeInTheDocument();
   });
 });
