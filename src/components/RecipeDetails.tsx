9f90654d93a78077ede5a3345a963b88382c03ab
Rafael <rafaelmagalhaes1@live.com>
2023-10-27 15:27:07 -0300

    refact: fetch apis

--- src/components/RecipeDetails.tsx
+++ src/components/RecipeDetails.tsx
@@ -6,44 +6,51 @@ function RecipeDetails() {
 
   const [recipe, setRecipe] = useState<any>();
 
-  const fetchRecipeMeal = async () => {
-    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${52771}`);
-    const data = await response.json();
-    console.log(data.meals[0]);
-    setRecipe(data.meals[0]);
-  };
-
-  const fetchRecipeDrinks = async () => {
-    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
-    const data = await response.json();
-    console.log(data.drink[0]);
-    setRecipe(data.drink[0]);
-  };
-
   useEffect(() => {
-    fetchRecipeDrinks();
-    fetchRecipeMeal();
+    // Realize a requisição para a API de comidas
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setRecipe(data.meals[0]);
+        console.log(data.meals[0]);
+      })
+      .catch((error) => {
+        console.error('Erro na requisição de comidas: ', error);
+      });
+
+    // Realize a requisição para a API de bebidas
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setRecipe(data.drinks[0]);
+      })
+      .catch((error) => {
+        console.error('Erro na requisição de bebidas: ', error);
+      });
   }, [id]);
 
+  /*
+    ok - A foto deve ter o atributo data-testid="recipe-photo".
+    ok - O título deve ter o atributo data-testid="recipe-title".
+    ok - O texto da categoria deve ter o atributo data-testid="recipe-category".
+    Os ingredientes devem ter o atributo data-testid="${index}-ingredient-name-and-measure".
+    O texto de instruções deve ter o atributo data-testid="instructions".
+    O vídeo, presente somente na tela de comidas, deve ter o atributo data-testid="video".
+ */
   return (
     <div>
       <div>
         {recipe ? (
           <div>
-            <h2>{recipe.strMeals || recipe.strDrink}</h2>
-            {recipe.strMeals ? (
-              <img
-                data-testid="recipe-photo"
-                src={ recipe.strMealThumb }
-                alt={ recipe.strMeals }
-              />
-            ) : (
-              <img
-                data-testid="recipe-photo"
-                src={ recipe.strDrinkThumb }
-                alt={ recipe.strDrink }
-              />
-            )}
+            <h1>{recipe.strMeals || recipe.strDrink}</h1>
+            <img
+              data-testid="recipe-photo"
+              src={ recipe.strMealThumb || recipe.strDrinkThumb }
+              alt="recipe"
+            />
+            <p data-testid="recipe-category">
+              { recipe.strCategory }
+            </p>
           </div>
         ) : (
           <p>Carregando detalhes da receita...</p>
