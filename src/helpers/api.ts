b3336bec30de8693f8256f1ad1df0c0d82061689
Rafael <rafaelmagalhaes1@live.com>
2023-10-28 10:02:11 -0300

    feat: create fetch recipes recommendations

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -31,10 +31,10 @@ export const fetchSearchFirtsLetter = async (type: string, search: string) => {
   }
 };
 
-export const fetchRecipeDetails = async (id: string, isMealsPage: boolean) => {
-  const API_URL = isMealsPage
-    ? `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
-    : `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`;
+export const fetchRecipeDetails = async (id: string, isDrinksPage: boolean) => {
+  const API_URL = isDrinksPage
+    ? `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
+    : `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
 
   try {
     const response = await fetch(API_URL);
@@ -42,5 +42,19 @@ export const fetchRecipeDetails = async (id: string, isMealsPage: boolean) => {
     return data.drinks ? data.drinks[0] : data.meals[0];
   } catch (error) {
     throw new Error('Erro na requisição da receita');
+  }
+};
+
+export const fetchRecommendations = async (type: string) => {
+  try {
+    const response = await fetch(
+      `https://www.${type === 'meals' ? 'thecocktaildb' : 'themealdb'}.com/api/json/v1/1/search.php?s=`,
+    );
+    const data = await response.json();
+    return type === 'meals' ? data.drinks : data.meals;
+  } catch (error) {
+    console.error(`Erro na requisição de
+    ${type === 'meals' ? 'comidas' : 'bebidas'}: `, error);
+    return [];
   }
 };
