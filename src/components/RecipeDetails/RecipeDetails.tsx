c99766eaccd2445af4051b79e058a3907eb35255
Rafael <rafaelmagalhaes1@live.com>
2023-10-30 07:54:14 -0300

    feat: requirements 24, 25, 26, 27, 28, 29, 30, 31, 32 e 33 ok

---
+++ src/components/RecipeDetails/RecipeDetails.tsx
@@ -0,0 +1,112 @@
+import { useEffect, useState } from 'react';
+import { useLocation, useParams } from 'react-router-dom';
+import { getIngredientsList } from '../../helpers/helpers';
+import { fetchRecipeDetails, fetchRecommendations } from '../../helpers/api';
+import FavoriteButton from './FavoriteButton/FavoriteButton';
+import ShareButton from './ShareButton/ShareButton';
+import StartRecipeButton from './StartRecipeButton/StartRecipeButton';
+import { ContainerRecipeDetails, Wrapper } from './Styles';
+
+function RecipeDetails() {
+  const location = useLocation();
+  const { id } = useParams<{ id: string }>();
+  const isDrinksPage = location.pathname.includes('drinks');
+
+  const [recipe, setRecipe] = useState<any>();
+  const [recommendations, setRecommendations] = useState<any[]>([]);
+
+  useEffect(() => {
+    if (!id) return;
+    const fetchData = async () => {
+      try {
+        const recipeData = await fetchRecipeDetails(id, isDrinksPage);
+        setRecipe(recipeData);
+
+        const data = await fetchRecommendations(isDrinksPage ? 'drinks' : 'meals');
+        setRecommendations(data.slice(0, 6));
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, [id, isDrinksPage]);
+
+  return (
+    <ContainerRecipeDetails>
+      <Wrapper>
+        {recipe ? (
+          <div>
+            <h1 data-testid="recipe-title">{recipe.strMeal || recipe.strDrink}</h1>
+            {console.log(recipe.strMeal || recipe.strDrink)}
+            <img
+              data-testid="recipe-photo"
+              src={ recipe.strMealThumb || recipe.strDrinkThumb }
+              alt="recipe"
+              width={ 300 }
+            />
+            <div style={ { display: 'flex', gap: '1rem', margin: '10px 0' } }>
+              <div style={ { margin: '10px 0' } }>
+                <FavoriteButton recipe={ recipe } />
+              </div>
+              <div style={ { margin: '10px 0' } }>
+                <ShareButton />
+              </div>
+            </div>
+            <p data-testid="recipe-category">{ recipe.strCategory }</p>
+            {recipe.strAlcoholic && (
+              <p data-testid="recipe-category">{ recipe.strAlcoholic }</p>
+            )}
+            <ul>
+              {getIngredientsList(recipe).map((ingredient, index) => (
+                <li
+                  key={ index }
+                  data-testid={ `${index}-ingredient-name-and-measure` }
+                >
+                  {ingredient}
+                </li>
+              ))}
+            </ul>
+            <p data-testid="instructions">{recipe.strInstructions}</p>
+            {recipe.strYoutube && (
+              <iframe
+                data-testid="video"
+                title="recipe-video"
+                width="100%"
+                height="315"
+                src={ recipe.strYoutube.replace('watch?v=', 'embed/') }
+                frameBorder="0"
+                allowFullScreen
+              />
+            )}
+            <section>
+              <h2>Receitas recomendadas</h2>
+              <div style={ { overflowX: 'auto', display: 'flex', width: '100%' } }>
+                {recommendations.map((recommendation, index) => (
+                  <div
+                    key={ recommendation.idDrink || recommendation.idMeal }
+                    data-testid={ `${index}-recommendation-card` }
+                    style={ {
+                      flex: '0 0 auto',
+                      width: '140px',
+                      border: '1px solid #ccc',
+                      margin: '5px',
+                    } }
+                  >
+                    <h3 data-testid={ `${index}-recommendation-title` }>
+                      {recommendation.strDrink || recommendation.strMeal}
+                    </h3>
+                  </div>
+                ))}
+              </div>
+            </section>
+            <StartRecipeButton />
+          </div>
+        ) : (
+          <p>Carregando detalhes da receita...</p>
+        )}
+      </Wrapper>
+    </ContainerRecipeDetails>
+  );
+}
+
+export default RecipeDetails;
