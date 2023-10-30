af350639d461b4e0bd19a962dc770f7f714eb28e
Rafael <rafaelmagalhaes1@live.com>
2023-10-30 07:50:04 -0300

    feat: initial styles recipe details

---
+++ src/components/RecipeDetails/Styles.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const ContainerRecipeDetails = styled.section`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: auto;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 30px;
+  width: 360px;
+  height: 640px;
+`;
