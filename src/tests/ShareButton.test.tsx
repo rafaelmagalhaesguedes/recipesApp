dce9668daf35748c7469d65e72607cca85ac09bf
Rafael <rafaelmagalhaes1@live.com>
2023-11-07 12:32:50 -0300

    feat: create tests to share button

---
+++ src/tests/ShareButton.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react';
+import { vi } from 'vitest';
+import userEvent from '@testing-library/user-event';
+import ShareButton from '../components/ShareButton';
+
+describe('ShareButton', () => {
+  it('renders correctly', () => {
+    render(<ShareButton />);
+
+    const button = screen.getByTestId('share-btn');
+    expect(button).toBeInTheDocument();
+  });
+
+  it('displays a message after the link is copied', async () => {
+    const mockClipboard = { writeText: vi.fn() };
+    Object.defineProperty(global.navigator, 'clipboard', { value: mockClipboard, writable: true });
+
+    render(<ShareButton />);
+
+    const button = screen.getByTestId('share-btn');
+    userEvent.click(button);
+
+    const message = await screen.findByText('Link copied!');
+    expect(message).toBeInTheDocument();
+  });
+});
