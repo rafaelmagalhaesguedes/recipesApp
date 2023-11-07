45872d667880132d49ad5d94735c2e4cd8e79326
Rafael <rafaelmagalhaes1@live.com>
2023-11-07 12:32:02 -0300

    feat: create tests to recipe card

---
+++ src/tests/RecipeCard.test.tsx
@@ -0,0 +1,30 @@
+import { render, screen } from '@testing-library/react';
+import RecipeCard from '../components/RecipeCard';
+
+describe('RecipeCard', () => {
+  const mockDrink: any = {
+    strDrink: 'Cocktail',
+    strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/ustsqw1468250014.jpg',
+  };
+
+  const mockMeal: any = {
+    strMeal: 'Pasta',
+    strMealThumb: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
+  };
+
+  it('renders correctly with drink data', () => {
+    render(<RecipeCard cardIndex={ 0 } recipe={ mockDrink } />);
+
+    expect(screen.getByTestId('0-card-img')).toHaveAttribute('src', mockDrink.strDrinkThumb);
+    expect(screen.getByTestId('0-card-img')).toHaveAttribute('alt', mockDrink.strDrink);
+    expect(screen.getByTestId('0-card-name')).toHaveTextContent(mockDrink.strDrink);
+  });
+
+  it('renders correctly with meal data', () => {
+    render(<RecipeCard cardIndex={ 1 } recipe={ mockMeal } />);
+
+    expect(screen.getByTestId('1-card-img')).toHaveAttribute('src', mockMeal.strMealThumb);
+    expect(screen.getByTestId('1-card-img')).toHaveAttribute('alt', mockMeal.strMeal);
+    expect(screen.getByTestId('1-card-name')).toHaveTextContent(mockMeal.strMeal);
+  });
+});
