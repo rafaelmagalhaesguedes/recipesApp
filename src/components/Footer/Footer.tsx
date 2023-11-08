cb11ebb8d67b31c1aba7cf719c673162c91da8ad
Rafael <rafaelmagalhaes1@live.com>
2023-11-07 22:42:56 -0300

    feat: footer styled

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,28 +1,26 @@
 import { Link } from 'react-router-dom';
-import drinkIcon from '../../images/drinkIcon.svg';
-import mealIcon from '../../images/mealIcon.svg';
-import './Footer.css';
+import drinkIcon from '../../images/iconDrink.png';
+import mealIcon from '../../images/iconDish.png';
+import { FooterContainer } from './Styles';
 
 function Footer() {
   return (
-    <div className="footer" data-testid="footer">
-      <div>
-        <Link to="/drinks">
-          <img
-            data-testid="drinks-bottom-btn"
-            src={ drinkIcon }
-            alt="Bebidas"
-          />
-        </Link>
-        <Link to="/meals">
-          <img
-            data-testid="meals-bottom-btn"
-            src={ mealIcon }
-            alt="Comidas"
-          />
-        </Link>
-      </div>
-    </div>
+    <FooterContainer data-testid="footer">
+      <Link to="/drinks">
+        <img
+          data-testid="drinks-bottom-btn"
+          src={ drinkIcon }
+          alt="Bebidas"
+        />
+      </Link>
+      <Link to="/meals">
+        <img
+          data-testid="meals-bottom-btn"
+          src={ mealIcon }
+          alt="Comidas"
+        />
+      </Link>
+    </FooterContainer>
   );
 }
 export default Footer;
---
+++ src/components/Footer/Styles.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const FooterContainer = styled.footer`
+  bottom: 0;
+  position: fixed;
+  background: #41197F;
+  width: 360px;
+  height: 50px;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 0 25px;
+  align-items: center;
+`;
