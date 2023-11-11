92eb52ea381a8b7e46aac6f2efd1743177a168e2
Rafael <rafaelmagalhaes1@live.com>
2023-11-11 09:36:46 -0300

    fix: heigth done recipes page

--- src/pages/DoneRecipes/Styles.tsx
+++ src/pages/DoneRecipes/Styles.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const ContainerDoneRecipes = styled.section`
   width: 100%;
-  height: auto;
+  height: 100vh;
   display: flex;
   justify-content: center;
 
