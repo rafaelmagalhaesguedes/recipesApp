81349ed57a46920237340312d40b49d0e7b292f2
Rafael <rafaelmagalhaes1@live.com>
2023-11-12 09:21:21 -0300

    fix: position footer

--- src/components/Footer/Styles.tsx
+++ src/components/Footer/Styles.tsx
@@ -7,9 +7,6 @@ export const FooterContainer = styled.footer`
     width: 100%;
     height: 200px;
     color: darkgray;
-    position: absolute;
-    bottom: 0;
-    left: 0;
 
     display: flex;
     flex-direction: row;
--- src/pages/RecipeDetails/Styles.tsx
+++ src/pages/RecipeDetails/Styles.tsx
@@ -5,7 +5,6 @@ export const ContainerRecipeDetails = styled.section`
   flex-direction: column;
   align-items: center;
   width: 100%;
-  height: 100%;
   margin: 0;
 `;
 
