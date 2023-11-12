ca28e6d94ca8ff7f05c16fb26d2d8b2c6d4e2d48
Rafael <rafaelmagalhaes1@live.com>
2023-11-12 18:28:16 -0300

    feat: add button back to home on result search

--- src/components/SearchResult/SearchResult.tsx
+++ src/components/SearchResult/SearchResult.tsx
@@ -1,29 +1,55 @@
 import { Link } from 'react-router-dom';
 import { useContext } from 'react';
 import RecipesContext from '../../context/RecipesContext';
-import { ContainerSearchResult, ResultCard } from './Styles';
+import iconArrow from '../../images/arrowBack.png';
+import {
+  ContainerSearchResult,
+  ResultCard,
+  ResultsNotFound,
+  WrapperSearchResult,
+} from './Styles';
 
 function SearchResult() {
-  const { searchData } = useContext(RecipesContext);
+  const { searchData, search } = useContext(RecipesContext);
 
   return (
     <ContainerSearchResult>
-      <h1>Search results: </h1>
-      {searchData && searchData.slice(0, 12).map((el: any, index: any) => (
-        <ResultCard key={ index } data-testid={ `${index}-recipe-card` }>
-          <Link to={ `/meals/${el.idMeal}` }>
-            <img
-              src={ el.strMealThumb || el.strDrinkThumb }
-              alt={ el.strMeal || el.strDrink }
-              data-testid={ `${index}-card-img` }
-              width={ 100 }
-            />
-            <h2 data-testid={ `${index}-card-name` }>
-              {el.strMeal || el.strDrink}
-            </h2>
-          </Link>
-        </ResultCard>
-      ))}
+      <Link
+        to="/meals"
+        onClick={ window.location.reload }
+        className="iconArrow"
+      >
+        <img className="arrow" src={ iconArrow } alt="Back" />
+        <p>Back to Home</p>
+      </Link>
+      <h1>
+        Search result of
+        {' '}
+        {search}
+      </h1>
+      <WrapperSearchResult>
+        {searchData && searchData.slice(0, 12).map((el: any, index: any) => (
+          <ResultCard key={ index } data-testid={ `${index}-recipe-card` }>
+            <Link to={ `/meals/${el.idMeal}` }>
+              <img
+                src={ el.strMealThumb || el.strDrinkThumb }
+                alt={ el.strMeal || el.strDrink }
+                data-testid={ `${index}-card-img` }
+                width={ 100 }
+              />
+              <p data-testid={ `${index}-card-name` }>
+                {el.strMeal || el.strDrink}
+              </p>
+            </Link>
+          </ResultCard>
+        ))}
+      </WrapperSearchResult>
+
+      {searchData && searchData.length === 0 && (
+        <ResultsNotFound>
+          <h1>No results found</h1>
+        </ResultsNotFound>
+      )}
     </ContainerSearchResult>
   );
 }
--- src/components/SearchResult/Styles.tsx
+++ src/components/SearchResult/Styles.tsx
@@ -6,16 +6,58 @@ export const ContainerSearchResult = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  flex-direction: column;
-  margin-bottom: 50px;
+  flex-flow: column nowrap;
+  margin: 50px 0 70px 0;
+
+  .iconArrow {
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: flex-start;
+    width: 80%;
+    text-align: left;
+    padding: 0 50px;
+  }
+
+  .arrow {
+    width: 30px;
+  }
+
+  p {
+    padding: 10px;
+  }
 
   h1 {
-    margin: 20px 0;
+    margin-bottom: 20px;
+    font-style: italic;
+    text-align: center;
+  }
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    height: auto;
+    margin: 10px 0;
+
+    .iconArrow {
+      display: flex;
+      flex-flow: row nowrap;
+      justify-content: flex-start;
+      width: 100%;
+      text-align: left;
+      padding: 10px;
+      margin: 0;
+    }
   }
 `;
 
+export const WrapperSearchResult = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  align-items: center;
+  justify-content: center;
+`;
+
 export const ResultCard = styled.div`
-  width: 80%;
+  width: 360px;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -27,8 +69,29 @@ export const ResultCard = styled.div`
     border-radius: 10px;
   }
 
-  h2 {
+  p {
+    padding: 10px 0;
     word-wrap: break-word;
     width: 318px;
+  }
+
+  @media screen and (max-width: 768px) {
+    margin: 0;
+
+    p {
+      padding: 10px 0 30px 0;
+    }
+  }
+`;
+
+export const ResultsNotFound = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  margin-top: -50px;
+
+  h1 {
+    font-size: 30px;
   }
 `;
