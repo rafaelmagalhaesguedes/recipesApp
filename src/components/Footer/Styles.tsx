905288812cc4c0330689f0a0b887a5cee18e7e65
Rafael <rafaelmagalhaes1@live.com>
2023-11-12 09:20:13 -0300

    fix: footer position fixed bottom

--- src/components/Footer/Styles.tsx
+++ src/components/Footer/Styles.tsx
@@ -7,6 +7,9 @@ export const FooterContainer = styled.footer`
     width: 100%;
     height: 200px;
     color: darkgray;
+    position: absolute;
+    bottom: 0;
+    left: 0;
 
     display: flex;
     flex-direction: row;
