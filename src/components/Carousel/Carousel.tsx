6b5b6b63015073ae2fc24260a5b165ae5a5d9ada
Rafael <rafaelmagalhaes1@live.com>
2023-11-14 12:16:04 -0300

    refact: carousel details

---
+++ src/components/Carousel/Carousel.tsx
@@ -0,0 +1,63 @@
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { fetchRecommendations } from '../../helpers/api';
+import {
+  CarouselContainer,
+  CarouselTitle, ContainerCarouselDetails, RecommendationCard } from './Style';
+
+interface CarouselProps {
+  isDrinksPage: boolean;
+}
+
+function Carousel({ isDrinksPage }: CarouselProps) {
+  const [recommendations, setRecommendations] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchRecommendations(isDrinksPage ? 'drinks' : 'meals');
+        setRecommendations(data.slice(0, 6));
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, [isDrinksPage]);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  return (
+    <ContainerCarouselDetails>
+      <CarouselTitle>Recommended</CarouselTitle>
+      <CarouselContainer>
+        {recommendations.map((recommendation, index) => (
+          <RecommendationCard
+            key={ recommendation.idDrink || recommendation.idMeal }
+            data-testid={ `${index}-recommendation-card` }
+          >
+            <Link
+              to={ recommendation.idMeal ? `/meals/${recommendation.idMeal}`
+                : `/drinks/${recommendation.idDrink}` }
+              onClick={ scrollToTop }
+            >
+              <img
+                src={ recommendation.strDrinkThumb || recommendation.strMealThumb }
+                alt="recipe"
+              />
+              <h3 data-testid={ `${index}-recommendation-title` }>
+                {recommendation.strDrink || recommendation.strMeal}
+              </h3>
+            </Link>
+          </RecommendationCard>
+        ))}
+      </CarouselContainer>
+    </ContainerCarouselDetails>
+  );
+}
+
+export default Carousel;
