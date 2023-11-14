aecd25d897a5f0bfc561846d26702156696b9254
Rafael <rafaelmagalhaes1@live.com>
2023-11-14 12:17:38 -0300

    fix: container

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,31 @@
 import { Route, Routes } from 'react-router-dom';
 import Login from './pages/Login/Login';
-import Layout from './components/Layout';
+import Layout from './components/Layout/Layout';
 import Recipes from './pages/Recipes/Recipes';
 import RecipeDetails from './pages/RecipeDetails/RecipeDetails';
 import RecipeInProgress from './pages/RecipeInProgress/RecipeInProgress';
 import DoneRecipes from './pages/DoneRecipes/DoneRecipes';
 import Profile from './pages/Profile/Profile';
 import FavoriteRecipes from './pages/FavoriteRecipes/FavoriteRecipes';
-import { Container } from './styles/GlobalStyle';
 import PageNotFound from './pages/PageNotFound/PageNotFound';
 
 function App() {
   return (
-    <Container>
-      <Routes>
-        <Route path="/" element={ <Login /> } />
-        <Route path="/" element={ <Layout /> }>
-          <Route path="/meals" element={ <Recipes /> } />
-          <Route path="/drinks" element={ <Recipes /> } />
-          <Route path="/profile" element={ <Profile /> } />
-          <Route path="/done-recipes" element={ <DoneRecipes /> } />
-          <Route path="/favorite-recipes" element={ <FavoriteRecipes /> } />
-          <Route path="/meals/:id" element={ <RecipeDetails /> } />
-          <Route path="/drinks/:id" element={ <RecipeDetails /> } />
-          <Route path="/meals/:id/in-progress" element={ <RecipeInProgress /> } />
-          <Route path="/drinks/:id/in-progress" element={ <RecipeInProgress /> } />
-        </Route>
-        <Route path="*" element={ <PageNotFound /> } />
-      </Routes>
-    </Container>
+    <Routes>
+      <Route path="/" element={ <Login /> } />
+      <Route path="/" element={ <Layout /> }>
+        <Route path="/meals" element={ <Recipes /> } />
+        <Route path="/drinks" element={ <Recipes /> } />
+        <Route path="/profile" element={ <Profile /> } />
+        <Route path="/done-recipes" element={ <DoneRecipes /> } />
+        <Route path="/favorite-recipes" element={ <FavoriteRecipes /> } />
+        <Route path="/meals/:id" element={ <RecipeDetails /> } />
+        <Route path="/drinks/:id" element={ <RecipeDetails /> } />
+        <Route path="/meals/:id/in-progress" element={ <RecipeInProgress /> } />
+        <Route path="/drinks/:id/in-progress" element={ <RecipeInProgress /> } />
+      </Route>
+      <Route path="*" element={ <PageNotFound /> } />
+    </Routes>
   );
 }
 export default App;
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,14 +1,15 @@
 import { Outlet } from 'react-router-dom';
-import Footer from './Footer/Footer';
-import Header from './Header/Header';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+import { Container } from './Styles';
 
 function Layout() {
   return (
-    <>
+    <Container>
       <Header />
       <Outlet />
       <Footer />
-    </>
+    </Container>
   );
 }
 
